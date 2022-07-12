1e932fc172fdff31bf0730ac5538480003d0a81b
AlamoGaston <103082945+AlamoGaston@users.noreply.github.com>
2022-07-11 23:48:59 -0300

    Update miarchivo.js

--- JS/miarchivo.js
+++ JS/miarchivo.js
@@ -1,45 +1,57 @@
+let user = prompt("Por favor ingrese su nombre de usuario: ");
+alert(
+  `Hola ${user.toUpperCase()}, usted ingreso al cotizador vehicular, por favor elija su tipo de vehiculo`
+);
+
 const listaDeSeguros = [
   {
     id: 1,
     nombre: "MotoRes",
     tipo: "Responsavilidad civil",
     precio: "500",
+    opcion: "Opcion 1",
   },
   {
     id: 2,
     nombre: "MotoCon",
     tipo: "Contra todo Riesgo",
     precio: "1000",
+    opcion: "Opcion 2",
   },
   {
     id: 3,
     nombre: "AutoRes",
     tipo: "Responsavilidad civil",
     precio: "750",
+    opcion: "Opcion 3",
   },
   {
     id: 4,
     nombre: "AutoCon",
     tipo: "Contra todo Riesgo",
     precio: "2000",
+    opcion: "Opcion 4",
   },
   {
     id: 5,
     nombre: "CamionRes",
     tipo: "Responsavilidad civil",
     precio: "1000",
+    opcion: "Opcion 5",
   },
   {
     id: 6,
     nombre: "CamionCon",
     tipo: "Contra todo Riesgo",
     precio: "5000",
+    opcion: "Opcion 6",
   },
   {
     id: 7,
     nombre: "AcopladoCon",
     tipo: "Responsavilidad civil",
     precio: "700",
+    opcion: "Opcion 7",
   },
 ];
 
@@ -50,20 +62,21 @@ for (const seguro of listaDeSeguros) {
   columna.className = "col-md-3 mt-5";
   columna.id = `columna-${seguro.id}`;
   columna.innerHTML = `
-        <div class="card">
-            <div class="card-body">
-                <p class="card-text">Nombre: <b>${seguro.nombre}</b></p>
-                <p class="card-text">Tipo: <b>${seguro.tipo}</b></p>
-                <p class="card-text">Precio venta: <b>${seguro.precio}</b></p>
-            </div>    
-        </div>
-    `;
+          <div class="card">
+              <div class="card-body">
+                  <p class="card-text">Nombre: <b>${seguro.nombre}</b></p>
+                  <p class="card-text">Tipo: <b>${seguro.tipo}</b></p>
+                  <p class="card-text">Precio venta: <b>${seguro.precio}</b></p>
+              </div>  
+              <button id="btn">Elegir: <b>${seguro.opcion}<b/></button>  
+          </div>
+      `;
 
   seguros.append(columna);
 }
 
 let columnaBorrar = document.getElementById("columna-7");
-columnaBorrar.remove();
+columnaBorrar.click();
 
 let header1 = document.getElementById("titulo");
 header1.innerHTML = "<h1>SEGUROS AUTOMOTOR</h1> <p>Elija su seguro</p>";
